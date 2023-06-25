a12a71e4b53ae3d65391a9f608ccfdedb41cec70
dooji2 <cmir79411@gmail.com>
2023-06-25 16:48:11 +0100

    Latest Update

--- flash_rpc.py
+++ flash_rpc.py
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/dooji2
 # Discord: dooji_
 
-# Last Modified: ‎Sunday, ‎June ‎25, ‎2023, ‏‎3:33:09 PM
+# Last Modified: ‎Sunday, ‎June ‎25, ‎2023, ‏‎4:47:37 PM
 # Modified By: Dooji (doojisbasement@gmail.com)
 
 # Copyright (c) 2023 Dooji
@@ -14,13 +14,15 @@ import os
 import psutil
 from pypresence import Presence
 import pystray
-from PIL import Image
+from PIL import Image, ImageTk
 import threading
 import sys
 import ctypes
 import base64
 import tempfile
-
+import tkinter as tk
+from tkinter import messagebox
+from queue import Queue
 
 if getattr(sys, 'frozen', False):
     # The application is running in a bundle
@@ -30,7 +32,7 @@ else:
     bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
 icon_path = os.path.join(bundle_dir, "icon.png")
-
+dooji_path = os.path.join(bundle_dir, "dooji.png")
 
 client_id = "1122093746402627604"
 RPC = Presence(client_id)
@@ -71,10 +73,6 @@ def recheck(game_name):
                 elif game_name == "papastacomia.swf":
                     RPC.update(details="Playing Papa's Taco Mia!", large_image="taco", small_image="flash")
 
-def on_quit_callback(icon, item):
-    icon.stop()
-    RPC.close()
-
 def run_script():
     while True:
         flash_found = False
@@ -89,19 +87,85 @@ def run_script():
 
         if not flash_found:
             RPC.clear()
-        time.sleep(5)  
+        try:
+            time.sleep(5)
+        except KeyboardInterrupt:
+            break
+
+# def create_about_window():
+#     about_window = tk.Toplevel()
+#     about_window.title("About Flash RPC")
+#     about_window.geometry("300x200")
+
+#     # Load and display the icon image
+#     icon_image = Image.open(dooji_path)
+#     icon_image = icon_image.resize((100, 100))  # Adjust the size as needed
+#     icon_photo = ImageTk.PhotoImage(icon_image)
+#     icon_label = tk.Label(about_window, image=icon_photo)
+#     icon_label.pack(pady=20)
+
+#     # Display the program credits
+#     credits_label = tk.Label(about_window, text="Author: Dooji\nVersion: 1.0")
+#     credits_label.pack()
+
+#     # Make the about window visible
+#     about_window.deiconify()
+
+def on_quit_callback(icon):
+    icon.stop()
+    RPC.close()
+    rpc_thread.join()  # Wait for the rpc_thread to finish
+    try:
+        sys.exit()  # Exit the Python interpreter
+    except SystemExit:
+        pass
+
+def create_about_window():
+    about_window = tk.Tk()
+    about_window.title("About Flash RPC")
+    about_window.geometry("300x200")
+    about_window.resizable(False, False)  # Disable window resizing
+
+    # Load and resize the image
+    image = Image.open(dooji_path)
+    image = image.resize((100, 100), Image.LANCZOS)
+    photo = ImageTk.PhotoImage(image)
+
+    # Create a label to display the image
+    label = tk.Label(about_window, image=photo)
+    label.image = photo  # Keep a reference to prevent image from being garbage collected
+    label.pack(pady=20)
+
+    # Display the program credits
+    credits_label = tk.Label(about_window, text="Author: Dooji\nVersion: 1.0.3c")
+    credits_label.pack()
+
+    # Center the window on the screen
+    about_window.eval('tk::PlaceWindow . center')
+
+    # Start the Tkinter event loop for the about window
+    about_window.mainloop()
 
 def create_system_tray_icon():
     image = Image.open(icon_path)
+
     menu = (
+        pystray.MenuItem(
+            "About",
+            lambda: create_about_window(),
+        ),
         pystray.MenuItem("Exit Flash RPC", on_quit_callback),
     )
+
     icon = pystray.Icon("Flash RPC", image, "Flash RPC", menu)
     return icon
 
 if __name__ == "__main__":
     icon = create_system_tray_icon()
-    thread = threading.Thread(target=run_script)
-    thread.start()
-    icon.run()
 
+    # Start the RPC thread
+    rpc_thread = threading.Thread(target=run_script)
+    rpc_thread.start()
+
+    # Run the system tray icon
+    icon.run()
