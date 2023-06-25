06a23eb5ba885174e4f2f7ff0cc6aedd3d8ee84c
dooji2 <cmir79411@gmail.com>
2023-06-25 08:52:35 +0100

    Fixed version!

--- flash_rpc.py
+++ flash_rpc.py
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/dooji2
 # Discord: dooji_
 
-# Last Modified: ‎Saturday, ‎June ‎24, ‎2023, ‏‎8:58:37 PM
+# Last Modified: ‎Saturday, ‎June ‎24, ‎2023, ‏‎8:28:34 PM
 # Modified By: Dooji (doojisbasement@gmail.com)
 
 # Copyright (c) 2023 Dooji
@@ -14,7 +14,26 @@ import os
 from pypresence import Presence
 import psutil
 
-def recheck():
+# Initialize the RPC client with your application's client ID
+client_id = "1122093746402627604"
+RPC = Presence(client_id)
+RPC.connect()
+
+for process in psutil.process_iter():
+        if "flash" in process.name(): 
+            # Flash process found, get the full command line
+            flash_command = " ".join(process.cmdline())
+            if "swf" in flash_command:
+                game_name = flash_command.split(os.path.sep)[-1]
+        else: 
+            RPC.clear()
+            
+
+
+
+
+
+def recheck(game_name):
     for process in psutil.process_iter():
         if "flash" in process.name(): 
             # Flash process found, get the full command line
@@ -22,69 +41,65 @@ def recheck():
             if "swf" in flash_command:
                 game_name = flash_command.split(os.path.sep)[-1]
                 if game_name == "papaspizzeria_v2.swf":
-                    game_name = "Papa's Pizzeria"
-                    lg = "pizza"
-                if game_name == "papaswingeria.swf":
-                    game_name = "Papa's Wingeria"
-                    lg = "wing"
-                if game_name == "papasbakeria.swf":
-                    game_name = "Papa's Bakeria"
-                    lg = "bake"
-                if game_name == "papasburgeria.swf":
-                    game_name = "Papa's Burgeria"
-                    lg = "burger"
-                if game_name == "papascheeseria_102.swf":
-                    game_name = "Papa's Cheeseria"
-                    lg = "cheese"
-                if game_name == "papascupcakeria.swf":
-                    game_name = "Papa's Cupcakeria"
-                    lg = "cupcake"
-                if game_name == "papasdonuteria.swf":
-                    game_name = "Papa's Donuteria"
-                    lg = "donut"
-                if game_name == "papasfreezeria.swf":
-                    game_name = "Papa's Freezeria"
-                    lg = "freezeria"
-                if game_name == "papashotdoggeria.swf":
-                    game_name = "Papa's Hot Doggeria"
-                    lg = "hotdog"
-                if game_name == "papaspancakeria.swf":
-                    game_name = "Papa's Pancakeria"
-                    lg = "pancake"
-                if game_name == "papaspastaria.swf":
-                    game_name = "Papa's Pastaria"
-                    lg = "pasta"
-                if game_name == "papasscooperia_v102.swf":
-                    game_name = "Papa's Scooperia"
-                    lg = "scoop"
-                if game_name == "papassushiria.swf":
-                    game_name = "Papa's Sushiria"
-                    lg = "sushi"
-                if game_name == "papastacomia.swf":
-                    game_name = "Papa's Taco Mia!"
-                    lg = "taco"
-        else: 
-            game_name = "Adobe Flash"
-            lg = "noflash"
-    # Initialize the RPC client with your application's client ID
-    client_id = "1122093746402627604"
-    RPC = Presence(client_id)
-    RPC.connect()
+                    
+                    
+                    RPC.update(details="Playing Papa's Pizzeria", large_image="pizza", small_image="flash")
+                    
+                elif game_name == "papaswingeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Wingeria", large_image="wing", small_image="flash")
+                elif game_name == "papasbakeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Bakeria", large_image="bake", small_image="flash")
+                elif game_name == "papasburgeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Burgeria", large_image="burger", small_image="flash")
+                elif game_name == "papascheeseria_102.swf":
+                    
+                    RPC.update(details="Playing Papa's Cheeseria", large_image="cheese", small_image="flash")
+                elif game_name == "papascupcakeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Cupcakeria", large_image="cupcake", small_image="flash")
+                elif game_name == "papasdonuteria.swf":
+                    
+                    RPC.update(details="Playing Papa's Donuteria", large_image="donut", small_image="flash")
+                elif game_name == "papasfreezeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Freezeria", large_image="freezeria", small_image="flash")
+                elif game_name == "papashotdoggeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Hot Doggeria", large_image="hotdog", small_image="flash")
+                elif game_name == "papaspancakeria.swf":
+                    
+                    RPC.update(details="Playing Papa's Pancakeria", large_image="pancake", small_image="flash")
+                elif game_name == "papaspastaria.swf":
+                    
+                    RPC.update(details="Playing Papa's Pastaria", large_image="pasta", small_image="flash")
+                elif game_name == "papasscooperia_v102.swf":
+                    
+                    RPC.update(details="Playing Papa's Scooperia", large_image="scoop", small_image="flash")
+                elif game_name == "papassushiria.swf":
+                    
+                    RPC.update(details="Playing Papa's Sushiria", large_image="sushi", small_image="flash")
+                elif game_name == "papastacomia.swf":
+                    
+                    RPC.update(details="Playing Papa's Taco Mia!", large_image="taco", small_image="flash")
+        
+                
+            
+                    
 
-    # Update the Rich Presence data
-    RPC.update(
-        details="Playing" + " " + game_name,
-        large_image=lg,
-        small_image="flash",
-        start=int(time.time())
-    )
+# Update the Rich Presence data
+#RPC.update(
+#    small_image="flash"
+#)
 
-    # Keep the script running
-    while True:
-        time.sleep(15)  # Update the presence every 15 seconds
-        RPC.update(state="No game has been loaded yet.")
+# Keep the script running
+while True:
+    if "flash" not in process.name(): RPC.clear()
+    recheck(game_name)
+    time.sleep(5)  # Update the presence every 15 seconds
+    
+# Disconnect the RPC client when done
+RPC.close()
 
-    # Disconnect the RPC client when done
-    RPC.close()
-
-recheck()
