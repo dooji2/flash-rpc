f021cc87d05ec0f1ff41797be62a8d993594ec47
dooji2 <cmir79411@gmail.com>
2023-06-25 17:24:03 +0100

    Version 1.0.3c

--- flash_rpc.py
+++ flash_rpc.py
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/dooji2
 # Discord: dooji_
 
-# Last Modified: ‎Sunday, ‎June ‎25, ‎2023, ‏‎4:47:37 PM
+# Last Modified: ‎Sunday, ‎June ‎25, ‎2023, ‏‎5:23:18 PM
 # Modified By: Dooji (doojisbasement@gmail.com)
 
 # Copyright (c) 2023 Dooji
@@ -20,15 +20,14 @@ import sys
 import ctypes
 import base64
 import tempfile
+import asyncio
 import tkinter as tk
 from tkinter import messagebox
 from queue import Queue
 
 if getattr(sys, 'frozen', False):
-    # The application is running in a bundle
     bundle_dir = sys._MEIPASS
 else:
-    # The application is running in the normal Python environment
     bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
 icon_path = os.path.join(bundle_dir, "icon.png")
@@ -97,53 +96,45 @@ def run_script():
 #     about_window.title("About Flash RPC")
 #     about_window.geometry("300x200")
 
-#     # Load and display the icon image
+#     
 #     icon_image = Image.open(dooji_path)
-#     icon_image = icon_image.resize((100, 100))  # Adjust the size as needed
+#     icon_image = icon_image.resize((100, 100))  
 #     icon_photo = ImageTk.PhotoImage(icon_image)
 #     icon_label = tk.Label(about_window, image=icon_photo)
 #     icon_label.pack(pady=20)
 
-#     # Display the program credits
+#     
 #     credits_label = tk.Label(about_window, text="Author: Dooji\nVersion: 1.0")
 #     credits_label.pack()
 
-#     # Make the about window visible
+#     
 #     about_window.deiconify()
 
 def on_quit_callback(icon):
     icon.stop()
     RPC.close()
-    rpc_thread.join()  # Wait for the rpc_thread to finish
-    try:
-        sys.exit()  # Exit the Python interpreter
-    except SystemExit:
-        pass
+    os._exit(0)
+
 
 def create_about_window():
     about_window = tk.Tk()
     about_window.title("About Flash RPC")
     about_window.geometry("300x200")
-    about_window.resizable(False, False)  # Disable window resizing
+    about_window.resizable(False, False)  
 
-    # Load and resize the image
-    image = Image.open(dooji_path)
-    image = image.resize((100, 100), Image.LANCZOS)
-    photo = ImageTk.PhotoImage(image)
+    imagei = Image.open(dooji_path)
+    imagei = imagei.resize((100, 100), Image.LANCZOS)
+    photo = ImageTk.PhotoImage(imagei)
 
-    # Create a label to display the image
     label = tk.Label(about_window, image=photo)
-    label.image = photo  # Keep a reference to prevent image from being garbage collected
+    label.imagei = photo  
     label.pack(pady=20)
 
-    # Display the program credits
     credits_label = tk.Label(about_window, text="Author: Dooji\nVersion: 1.0.3c")
     credits_label.pack()
 
-    # Center the window on the screen
     about_window.eval('tk::PlaceWindow . center')
 
-    # Start the Tkinter event loop for the about window
     about_window.mainloop()
 
 def create_system_tray_icon():
@@ -163,9 +154,7 @@ def create_system_tray_icon():
 if __name__ == "__main__":
     icon = create_system_tray_icon()
 
-    # Start the RPC thread
     rpc_thread = threading.Thread(target=run_script)
     rpc_thread.start()
 
-    # Run the system tray icon
     icon.run()
